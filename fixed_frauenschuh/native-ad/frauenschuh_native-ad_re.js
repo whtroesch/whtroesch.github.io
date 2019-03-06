c3a9d81616e28f8c96174d934f71f92a0d044e4f
whtroesch <40169023+whtroesch@users.noreply.github.com>
2019-03-06 14:03:32 +0100

    gwrrh
    hteh

--- fixed_frauenschuh/native-ad/frauenschuh_native-ad_re.js
+++ fixed_frauenschuh/native-ad/frauenschuh_native-ad_re.js
@@ -3,7 +3,7 @@
 var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
 lib.ssMetadata = [
-		{name:"frauenschuh_native_ad_re_atlas_", frames: [[641,1368,639,150],[1282,1368,639,150],[1282,760,639,150],[641,912,639,150],[1282,912,639,150],[1282,152,639,150],[0,152,639,150],[641,152,639,150],[641,0,639,150],[0,304,639,150],[1282,304,639,150],[0,0,639,150],[1282,1216,639,150],[1282,0,639,150],[641,304,639,150],[641,608,639,150],[1282,608,639,150],[641,760,639,150],[0,760,639,150],[0,456,639,150],[1282,456,639,150],[0,608,639,150],[641,456,639,150],[0,1368,639,150],[1282,1064,639,150],[0,1064,639,150],[0,912,639,150],[0,1216,639,150],[641,1064,639,150],[641,1216,639,150]]}
+		{name:"frauenschuh_native_ad_re_atlas_", frames: [[1052,202,524,200],[526,0,524,200],[1052,606,524,200],[0,808,524,200],[0,0,524,200],[0,1212,524,200],[526,1818,524,200],[526,1010,524,200],[0,1414,524,200],[526,808,524,200],[1052,1212,524,200],[1052,1414,524,200],[526,404,524,200],[1052,1818,524,200],[526,1212,524,200],[1052,1010,524,200],[0,1616,524,200],[0,1010,524,200],[1052,1616,524,200],[526,1414,524,200],[0,1818,524,200],[526,1616,524,200],[1052,808,524,200],[526,202,524,200],[1052,0,524,200],[1052,404,524,200],[0,202,524,200],[0,404,524,200],[0,606,524,200],[526,606,524,200]]}
 ];
 
 
@@ -11,314 +11,314 @@ lib.ssMetadata = [
 
 
 
-(lib.frauenschuh_nativead02_0 = function() {
+(lib.frauenschuh_nativead_mobile_0 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_1 = function() {
+(lib.frauenschuh_nativead_mobile_1 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(1);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_10 = function() {
+(lib.frauenschuh_nativead_mobile_10 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(2);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_11 = function() {
+(lib.frauenschuh_nativead_mobile_11 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(3);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_12 = function() {
+(lib.frauenschuh_nativead_mobile_12 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(4);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_13 = function() {
+(lib.frauenschuh_nativead_mobile_13 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(5);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_14 = function() {
+(lib.frauenschuh_nativead_mobile_14 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(6);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_15 = function() {
+(lib.frauenschuh_nativead_mobile_15 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(7);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_16 = function() {
+(lib.frauenschuh_nativead_mobile_16 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(8);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_17 = function() {
+(lib.frauenschuh_nativead_mobile_17 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(9);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_18 = function() {
+(lib.frauenschuh_nativead_mobile_18 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(10);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_19 = function() {
+(lib.frauenschuh_nativead_mobile_19 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(11);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_2 = function() {
+(lib.frauenschuh_nativead_mobile_2 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(12);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_20 = function() {
+(lib.frauenschuh_nativead_mobile_20 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(13);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_21 = function() {
+(lib.frauenschuh_nativead_mobile_21 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(14);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_22 = function() {
+(lib.frauenschuh_nativead_mobile_22 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(15);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_23 = function() {
+(lib.frauenschuh_nativead_mobile_23 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(16);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_24 = function() {
+(lib.frauenschuh_nativead_mobile_24 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(17);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_25 = function() {
+(lib.frauenschuh_nativead_mobile_25 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(18);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_26 = function() {
+(lib.frauenschuh_nativead_mobile_26 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(19);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_27 = function() {
+(lib.frauenschuh_nativead_mobile_27 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(20);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_28 = function() {
+(lib.frauenschuh_nativead_mobile_28 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(21);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_29 = function() {
+(lib.frauenschuh_nativead_mobile_29 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(22);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_3 = function() {
+(lib.frauenschuh_nativead_mobile_3 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(23);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_4 = function() {
+(lib.frauenschuh_nativead_mobile_4 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(24);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_5 = function() {
+(lib.frauenschuh_nativead_mobile_5 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(25);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_6 = function() {
+(lib.frauenschuh_nativead_mobile_6 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(26);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_7 = function() {
+(lib.frauenschuh_nativead_mobile_7 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(27);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_8 = function() {
+(lib.frauenschuh_nativead_mobile_8 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(28);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_9 = function() {
+(lib.frauenschuh_nativead_mobile_9 = function() {
 	this.spriteSheet = ss["frauenschuh_native_ad_re_atlas_"];
 	this.gotoAndStop(29);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.frauenschuh_nativead02_gif = function(mode,startPosition,loop) {
+(lib.frauenschuh_nativead_mobile_gif = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
 	// Ebene_1
-	this.instance = new lib.frauenschuh_nativead02_0();
+	this.instance = new lib.frauenschuh_nativead_mobile_0();
 	this.instance.parent = this;
 
-	this.instance_1 = new lib.frauenschuh_nativead02_1();
+	this.instance_1 = new lib.frauenschuh_nativead_mobile_1();
 	this.instance_1.parent = this;
 
-	this.instance_2 = new lib.frauenschuh_nativead02_2();
+	this.instance_2 = new lib.frauenschuh_nativead_mobile_2();
 	this.instance_2.parent = this;
 
-	this.instance_3 = new lib.frauenschuh_nativead02_3();
+	this.instance_3 = new lib.frauenschuh_nativead_mobile_3();
 	this.instance_3.parent = this;
 
-	this.instance_4 = new lib.frauenschuh_nativead02_4();
+	this.instance_4 = new lib.frauenschuh_nativead_mobile_4();
 	this.instance_4.parent = this;
 
-	this.instance_5 = new lib.frauenschuh_nativead02_5();
+	this.instance_5 = new lib.frauenschuh_nativead_mobile_5();
 	this.instance_5.parent = this;
 
-	this.instance_6 = new lib.frauenschuh_nativead02_6();
+	this.instance_6 = new lib.frauenschuh_nativead_mobile_6();
 	this.instance_6.parent = this;
 
-	this.instance_7 = new lib.frauenschuh_nativead02_7();
+	this.instance_7 = new lib.frauenschuh_nativead_mobile_7();
 	this.instance_7.parent = this;
 
-	this.instance_8 = new lib.frauenschuh_nativead02_8();
+	this.instance_8 = new lib.frauenschuh_nativead_mobile_8();
 	this.instance_8.parent = this;
 
-	this.instance_9 = new lib.frauenschuh_nativead02_9();
+	this.instance_9 = new lib.frauenschuh_nativead_mobile_9();
 	this.instance_9.parent = this;
 
-	this.instance_10 = new lib.frauenschuh_nativead02_10();
+	this.instance_10 = new lib.frauenschuh_nativead_mobile_10();
 	this.instance_10.parent = this;
 
-	this.instance_11 = new lib.frauenschuh_nativead02_11();
+	this.instance_11 = new lib.frauenschuh_nativead_mobile_11();
 	this.instance_11.parent = this;
 
-	this.instance_12 = new lib.frauenschuh_nativead02_12();
+	this.instance_12 = new lib.frauenschuh_nativead_mobile_12();
 	this.instance_12.parent = this;
 
-	this.instance_13 = new lib.frauenschuh_nativead02_13();
+	this.instance_13 = new lib.frauenschuh_nativead_mobile_13();
 	this.instance_13.parent = this;
 
-	this.instance_14 = new lib.frauenschuh_nativead02_14();
+	this.instance_14 = new lib.frauenschuh_nativead_mobile_14();
 	this.instance_14.parent = this;
 
-	this.instance_15 = new lib.frauenschuh_nativead02_15();
+	this.instance_15 = new lib.frauenschuh_nativead_mobile_15();
 	this.instance_15.parent = this;
 
-	this.instance_16 = new lib.frauenschuh_nativead02_16();
+	this.instance_16 = new lib.frauenschuh_nativead_mobile_16();
 	this.instance_16.parent = this;
 
-	this.instance_17 = new lib.frauenschuh_nativead02_17();
+	this.instance_17 = new lib.frauenschuh_nativead_mobile_17();
 	this.instance_17.parent = this;
 
-	this.instance_18 = new lib.frauenschuh_nativead02_18();
+	this.instance_18 = new lib.frauenschuh_nativead_mobile_18();
 	this.instance_18.parent = this;
 
-	this.instance_19 = new lib.frauenschuh_nativead02_19();
+	this.instance_19 = new lib.frauenschuh_nativead_mobile_19();
 	this.instance_19.parent = this;
 
-	this.instance_20 = new lib.frauenschuh_nativead02_20();
+	this.instance_20 = new lib.frauenschuh_nativead_mobile_20();
 	this.instance_20.parent = this;
 
-	this.instance_21 = new lib.frauenschuh_nativead02_21();
+	this.instance_21 = new lib.frauenschuh_nativead_mobile_21();
 	this.instance_21.parent = this;
 
-	this.instance_22 = new lib.frauenschuh_nativead02_22();
+	this.instance_22 = new lib.frauenschuh_nativead_mobile_22();
 	this.instance_22.parent = this;
 
-	this.instance_23 = new lib.frauenschuh_nativead02_23();
+	this.instance_23 = new lib.frauenschuh_nativead_mobile_23();
 	this.instance_23.parent = this;
 
-	this.instance_24 = new lib.frauenschuh_nativead02_24();
+	this.instance_24 = new lib.frauenschuh_nativead_mobile_24();
 	this.instance_24.parent = this;
 
-	this.instance_25 = new lib.frauenschuh_nativead02_25();
+	this.instance_25 = new lib.frauenschuh_nativead_mobile_25();
 	this.instance_25.parent = this;
 
-	this.instance_26 = new lib.frauenschuh_nativead02_26();
+	this.instance_26 = new lib.frauenschuh_nativead_mobile_26();
 	this.instance_26.parent = this;
 
-	this.instance_27 = new lib.frauenschuh_nativead02_27();
+	this.instance_27 = new lib.frauenschuh_nativead_mobile_27();
 	this.instance_27.parent = this;
 
-	this.instance_28 = new lib.frauenschuh_nativead02_28();
+	this.instance_28 = new lib.frauenschuh_nativead_mobile_28();
 	this.instance_28.parent = this;
 
-	this.instance_29 = new lib.frauenschuh_nativead02_29();
+	this.instance_29 = new lib.frauenschuh_nativead_mobile_29();
 	this.instance_29.parent = this;
 
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},63).to({state:[{t:this.instance_2}]},2).to({state:[{t:this.instance_3}]},2).to({state:[{t:this.instance_4}]},2).to({state:[{t:this.instance_5}]},2).to({state:[{t:this.instance_6}]},2).to({state:[{t:this.instance_7}]},2).to({state:[{t:this.instance_8}]},2).to({state:[{t:this.instance_9}]},2).to({state:[{t:this.instance_10}]},2).to({state:[{t:this.instance_11}]},63).to({state:[{t:this.instance_12}]},2).to({state:[{t:this.instance_13}]},2).to({state:[{t:this.instance_14}]},2).to({state:[{t:this.instance_15}]},2).to({state:[{t:this.instance_16}]},2).to({state:[{t:this.instance_17}]},2).to({state:[{t:this.instance_18}]},2).to({state:[{t:this.instance_19}]},2).to({state:[{t:this.instance_20}]},2).to({state:[{t:this.instance_21}]},63).to({state:[{t:this.instance_22}]},2).to({state:[{t:this.instance_23}]},2).to({state:[{t:this.instance_24}]},2).to({state:[{t:this.instance_25}]},2).to({state:[{t:this.instance_26}]},2).to({state:[{t:this.instance_27}]},2).to({state:[{t:this.instance_28}]},2).to({state:[{t:this.instance_29}]},2).wait(2));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},50).to({state:[{t:this.instance_2}]},2).to({state:[{t:this.instance_3}]},2).to({state:[{t:this.instance_4}]},2).to({state:[{t:this.instance_5}]},2).to({state:[{t:this.instance_6}]},2).to({state:[{t:this.instance_7}]},2).to({state:[{t:this.instance_8}]},2).to({state:[{t:this.instance_9}]},2).to({state:[{t:this.instance_10}]},2).to({state:[{t:this.instance_11}]},50).to({state:[{t:this.instance_12}]},2).to({state:[{t:this.instance_13}]},2).to({state:[{t:this.instance_14}]},2).to({state:[{t:this.instance_15}]},2).to({state:[{t:this.instance_16}]},2).to({state:[{t:this.instance_17}]},2).to({state:[{t:this.instance_18}]},2).to({state:[{t:this.instance_19}]},2).to({state:[{t:this.instance_20}]},2).to({state:[{t:this.instance_21}]},50).to({state:[{t:this.instance_22}]},2).to({state:[{t:this.instance_23}]},2).to({state:[{t:this.instance_24}]},2).to({state:[{t:this.instance_25}]},2).to({state:[{t:this.instance_26}]},2).to({state:[{t:this.instance_27}]},2).to({state:[{t:this.instance_28}]},2).to({state:[{t:this.instance_29}]},2).wait(2));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(0,0,639,150);
+p.nominalBounds = new cjs.Rectangle(0,0,524,200);
 
 
 // stage content:
@@ -326,19 +326,19 @@ p.nominalBounds = new cjs.Rectangle(0,0,639,150);
 	this.initialize(mode,startPosition,loop,{});
 
 	// Ebene_1
-	this.instance = new lib.frauenschuh_nativead02_gif();
+	this.instance = new lib.frauenschuh_nativead_mobile_gif();
 	this.instance.parent = this;
-	this.instance.setTransform(319.5,75,1,1,0,0,0,319.5,75);
+	this.instance.setTransform(293,100,1,1,0,0,0,293,100);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(319.5,75,639,150);
+p.nominalBounds = new cjs.Rectangle(262,100,524,200);
 // library properties:
 lib.properties = {
 	id: 'CA70BD926E1D2E47A1ADE1713E616293',
-	width: 639,
-	height: 150,
+	width: 524,
+	height: 200,
 	fps: 24,
 	color: "#FFFFFF",
 	opacity: 0.00,
