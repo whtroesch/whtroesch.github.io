0338e39144fab50cdb5d71e53dc83ac39285f396
whtroesch <40169023+whtroesch@users.noreply.github.com>
2022-03-04 09:52:39 +0100

    sdfgsdf
    gsdfgdf

--- beckspreview/index.js
+++ beckspreview/index.js
@@ -3,7 +3,7 @@
 var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
 lib.ssMetadata = [
-		{name:"index_atlas_1", frames: [[0,0,970,250],[0,252,970,250],[0,756,219,202],[0,504,970,250]]}
+		{name:"index_atlas_1", frames: [[993,0,970,250],[993,252,970,250],[0,0,991,925],[993,504,970,250]]}
 ];
 
 
@@ -130,7 +130,7 @@ if (reversed == null) { reversed = false; }
 
 	this._renderFirstFrame();
 
-}).prototype = getMCSymbolPrototype(lib.Symbol1, new cjs.Rectangle(0,0,219,202), null);
+}).prototype = getMCSymbolPrototype(lib.Symbol1, new cjs.Rectangle(0,0,991,925), null);
 
 
 // stage content:
@@ -155,14 +155,21 @@ if (reversed == null) { reversed = false; }
 	this.instance_1 = new lib.Symbol1();
 	this.instance_1.setTransform(119,109.2,0.0039,0.0039,0,0,0,101.6,88.9);
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(1).to({regX:109.5,regY:101,scaleX:0.0703,scaleY:0.0703,x:118.95},0).wait(1).to({scaleX:0.1367,scaleY:0.1367,x:118.85},0).wait(1).to({scaleX:0.2031,scaleY:0.2031},0).wait(1).to({scaleX:0.2696,scaleY:0.2696,x:118.75,y:109.15},0).wait(1).to({scaleX:0.336,scaleY:0.336,y:109.2},0).wait(1).to({scaleX:0.4024,scaleY:0.4024,x:118.65},0).wait(1).to({scaleX:0.4688,scaleY:0.4688},0).wait(1).to({scaleX:0.5352,scaleY:0.5352,x:118.55,y:109.15},0).wait(1).to({scaleX:0.6016,scaleY:0.6016,x:118.5},0).wait(1).to({scaleX:0.668,scaleY:0.668,x:118.45},0).wait(1).to({scaleX:0.7344,scaleY:0.7344,x:118.4},0).wait(1).to({scaleX:0.8008,scaleY:0.8008,x:118.35},0).wait(1).to({scaleX:0.8672,scaleY:0.8672,x:118.3},0).wait(1).to({scaleX:0.9336,scaleY:0.9336,x:118.25},0).wait(1).to({scaleX:1,scaleY:1,x:118.15,y:109.1},0).wait(110));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(1).to({regX:495.5,regY:462.5,scaleX:0.0191,scaleY:0.0191,x:120.35,y:110.35},0).wait(1).to({scaleX:0.0342,scaleY:0.0342,x:120.2,y:110.1},0).wait(1).to({scaleX:0.0493,scaleY:0.0493,x:120,y:109.85},0).wait(1).to({scaleX:0.0644,scaleY:0.0644,x:119.85,y:109.6},0).wait(1).to({scaleX:0.0795,scaleY:0.0795,x:119.7,y:109.35},0).wait(1).to({scaleX:0.0946,scaleY:0.0946,x:119.5,y:109.05},0).wait(1).to({scaleX:0.1098,scaleY:0.1098,x:119.35,y:108.75},0).wait(1).to({scaleX:0.1249,scaleY:0.1249,x:119.15,y:108.5},0).wait(1).to({scaleX:0.14,scaleY:0.14,x:119,y:108.25},0).wait(1).to({scaleX:0.1551,scaleY:0.1551,x:118.85,y:108},0).wait(1).to({scaleX:0.1702,scaleY:0.1702,x:118.65,y:107.7},0).wait(1).to({scaleX:0.1853,scaleY:0.1853,x:118.5,y:107.45},0).wait(1).to({scaleX:0.2004,scaleY:0.2004,x:118.3,y:107.2},0).wait(1).to({scaleX:0.2156,scaleY:0.2156,x:118.15,y:106.9},0).wait(1).to({scaleX:0.2307,scaleY:0.2307,x:118,y:106.7},0).wait(110));
 
 	// eck
 	this.shape = new cjs.Shape();
 	this.shape.graphics.f("#FFFFFF").s().p("AyoRgMAAAgi/MAlRAAAMAAAAi/g");
-	this.shape.setTransform(117.5,111.375);
+	this.shape.setTransform(116.5,122.875);
 
 	this.timeline.addTween(cjs.Tween.get(this.shape).wait(125));
+
+	// Ebene_1
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#FFFFFF").s().p("EhOWADPIAAmeMCctAAAIAAGeg");
+	this.shape_1.setTransform(499.675,233.7);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape_1).wait(125));
 
 	// text
 	this.instance_2 = new lib.Symbol3();
@@ -173,21 +180,20 @@ if (reversed == null) { reversed = false; }
 
 	// bg
 	this.instance_3 = new lib.bg();
-	this.instance_3.setTransform(1,0);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(125));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new lib.AnMovieClip();
-p.nominalBounds = new cjs.Rectangle(21.9,124.4,949.1,162.6);
+p.nominalBounds = new cjs.Rectangle(21.9,125,979.3000000000001,162);
 // library properties:
 lib.properties = {
 	id: '4A3608690A31ED4EA110DB545FB0E6E1',
 	width: 970,
 	height: 250,
 	fps: 25,
-	color: "#FFFFFF",
+	color: "#323232",
 	opacity: 1.00,
 	manifest: [
 		{src:"images/index_atlas_1.png", id:"index_atlas_1"}
